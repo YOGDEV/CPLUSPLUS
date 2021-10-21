08ca06636945d1f5598c06761d8e36fb7d908324
YOGESH SINGH YADAV <86778735+YOGDEV@users.noreply.github.com>
2021-10-21 20:26:48 +0530

    Create t3reverse.cpp
    reversing the string using a pre-defined function in the ALGORITHM header file.

---
+++ Practical6/t3reverse.cpp
@@ -0,0 +1,14 @@
+#include<algorithm>//this headerfile consists of reverse function...
+// using reverse()
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "yogesh";
+ 
+    // Reverse str[begin..end]
+    reverse(str.begin(), str.end());
+ 
+    cout << str;
+    return 0;
+}
